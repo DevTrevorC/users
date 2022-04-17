a9d24bc6bea811be5470d020b4fcb6cd6dee9f64
Trevor Cash <tjcashit@gmail.com>
2022-04-16 19:26:55 -0500

    Users can now be subitted and all users can be displayed

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,11 +1,24 @@
+import re
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
-# from user import user
+from flask_app.models.user import User
 
 @app.route('/')
 def showAllUsers():
-    return render_template("display_all.html")
+    users = User.get_all()
+    print(users)
+    return render_template("display_all.html", all_users = users)
 
 @app.route('/add')
 def addUser():
     return render_template("add_user.html")
+
+@app.route('/save', methods=["POST"])
+def saveUser():
+    data = {
+        "firstName": request.form["firstName"],
+        "lastName" : request.form["lastName"],
+        "email" : request.form["email"]
+    }
+    User.add_user(data)
+    return redirect('/')
